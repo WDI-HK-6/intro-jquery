af0fa6f613f4d621eb63086d6d4fffd3807e3a98
Harry Chen <harrychenca@gmail.com>
2014-11-27 14:41:48 +0800

    Initial Commit

---
+++ js/application.js
@@ -0,0 +1,7 @@
+// $("#name").text("Clement");
+
+// when the DOM is finished loading, it will actually tell you that it’s ready
+jQuery(document).ready(function(){
+  $("#name").text("Clement");
+  
+});
